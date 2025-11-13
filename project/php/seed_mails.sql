ac3e4e08b93783981d73289ddde00fd4d5320860
tklocshem <156997970+tklocshem@users.noreply.github.com>
2025-11-13 11:54:49 +0200

    Fix comment typo in seed_mails.sql
    Corrected the comment for clarity and fixed a typo in the file name.

--- project/php/seed_mails.sql
+++ project/php/seed_mails.sql
@@ -1,7 +1,7 @@
 INSERT INTO `mails` (`subject`, `body`, `sender_name`, `sender_email`) VALUES
 ('content', 'content', 'NameForExample', 'example@gmail.com');
 
-// recipients for the first email (id = 1), taken from the example.txt file
+-- ecipients for the first email (id = 1), taken from the email_schema_example.txt file
 INSERT INTO `mail_recipients` (`mail_id`, `name`, `email`) VALUES
 (1, 'NameForExample', 'example@gmail.com');
 
