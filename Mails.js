bf93ee24787e7571b557dc67ce16490ce0f223e5
PGurulev <PGurulev@uclan.ac.uk>
2025-11-15 12:58:11 +0200

    Testing the fetch implementation
    The fetch will be used in future to fetch data from .php file

--- Mails.js
+++ Mails.js
@@ -4,5 +4,11 @@ function GenerateMailsMenu()
     StartingContent.style.display = "none";
     var EmailContent = document.getElementById("GamePage");
     EmailContent.style.display = "flex";
-
+    var JsonInfo = JSONTransmitter("AssetsAndExampless/JsonFiles/real_emails.json");
+}
+async function JSONTransmitter(filename)
+{
+    let FetchedData = await fetch(filename);
+    let ParcedData = await FetchedData.json();
+    console.log(ParcedData);
 }
