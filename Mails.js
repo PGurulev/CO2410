18079bc1a65a387b9d241a7ac7f924e3a1f2c9d0
PGurulev <PGurulev@uclan.ac.uk>
2025-11-15 12:46:17 +0200

    Preparations for the mails generation
    Made small changes for the html file and started testing the html file

---
+++ Mails.js
@@ -0,0 +1,8 @@
+function GenerateMailsMenu()
+{
+    var StartingContent = document.getElementById("MainPage");
+    StartingContent.style.display = "none";
+    var EmailContent = document.getElementById("GamePage");
+    EmailContent.style.display = "flex";
+
+}
