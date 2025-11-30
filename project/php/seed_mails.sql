6609079796af0d4fc4c60e18cb319025d183bd88
tklocshem <156997970+tklocshem@users.noreply.github.com>
2025-11-30 16:26:44 +0200

    Add 'isFake' column

--- project/php/seed_mails.sql
+++ project/php/seed_mails.sql
@@ -1,8 +1,8 @@
-INSERT INTO `mails` (`subject`, `body`, `sender_name`, `sender_email`) VALUES
-('content', 'content', 'NameForExample', 'example@gmail.com');
+INSERT INTO `mails` (`subject`, `body`, `sender_name`, `sender_email`, `isFake`) VALUES
+('content', 'content', 'NameForExample', 'example@gmail.com', 'true');
 
 -- ecipients for the first email (id = 1), taken from the email_schema_example.txt file
-INSERT INTO `mail_recipients` (`mail_id`, `name`, `email`) VALUES
-(1, 'NameForExample', 'example@gmail.com');
+INSERT INTO `mail_recipients` (`mail_id`, `name`, `email`, `isFake`) VALUES
+(1, 'NameForExample', 'example@gmail.com', 'true');
 
 
