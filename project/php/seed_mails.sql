700e383f65f2502843c49ce5ca36c902aeb781ff
tklocshem <156997970+tklocshem@users.noreply.github.com>
2025-12-06 21:24:40 +0200

    Fix is_fake flag in parse_json.php and update project files

--- project/php/seed_mails.sql
+++ project/php/seed_mails.sql
@@ -1,8 +1,8 @@
-INSERT INTO `mails` (`subject`, `body`, `sender_name`, `sender_email`, `isFake`) VALUES
-('content', 'content', 'NameForExample', 'example@gmail.com', 'true');
+INSERT INTO `mails` (`subject`, `body`, `sender_name`, `sender_email`) VALUES
+('content', 'content', 'NameForExample', 'example@gmail.com');
 
--- ecipients for the first email (id = 1), taken from the email_schema_example.txt file
-INSERT INTO `mail_recipients` (`mail_id`, `name`, `email`, `isFake`) VALUES
-(1, 'NameForExample', 'example@gmail.com', 'true');
+// recipients for the first email (id = 1), taken from the example.txt file
+INSERT INTO `mail_recipients` (`mail_id`, `name`, `email`) VALUES
+(1, 'NameForExample', 'example@gmail.com');
 
 
