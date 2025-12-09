0af0a17af748660f499ab6d8c697a471d2149819
RCrbss <rommysgoog@gmail.com>
2025-12-09 10:29:28 +0200

    add comments

--- leaderboard.js
+++ leaderboard.js
@@ -1,19 +1,19 @@
 function loadLeaderboard() {
-    fetch("project/php/leaderboard.php")
+    fetch("project/php/leaderboard.php") //Refer from php part for logic part of js
         .then(res => res.json())
         .then(data => {
-            const list = data.players;
+            const list = data.players; //Create list of player's data for leaderboard
 
             let html = 
                 <h2 class="leaderboard-title">Leaderboard</h2>
-                <table class="leaderboard">
+                <table class="leaderboard"> //Add main values to display what result, who played and when
                     <tr>
                         <th>Player</th>
                         <th>Score</th>
                         <th>Timestamp<th>
                     </tr>
             ;
-
+            //Create the list for each player which played
             list.forEach(player => {
                 html += 
                     <tr>
@@ -26,10 +26,11 @@ function loadLeaderboard() {
 
             html += "</table>";
 
-            document.getElementById("leaderBoard").innerHTML = html;
+            document.getElementById("leaderBoard").innerHTML = html; //Add the user in leaderboard
         });
 }
 
 
 
 window.addEventListener("load", loadLeaderboard);
+
